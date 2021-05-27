ef6a7cd17be4e8efdd2857fe0ad0c8c8a3ad8265
David Luna <davidluna@mail.weber.edu>
2021-05-26 20:59:01 -0600

    Added some practice functions

---
+++ Functions.py
@@ -0,0 +1,17 @@
+def my_Function():
+    print('hello world from a function')
+my_Function()
+
+def myFunction():
+    print('hi from a function')
+myFunction()
+
+def anotherFunction():
+    print('this is a new function')
+anotherFunction()
+
+def waterbottle():
+    print('my water bottle is blue from a function')
+waterbottle()
+
+
